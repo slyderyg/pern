ec9d4cb76841bffb65f978925e590574e5910e7f
slyderyg <kprokofev88@icloud.com>
2023-12-23 19:08:59 +0300

    refresh token added

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,14 @@
 const userService = require('../service/user-service')
-
+const { validationResult } = require('express-validator')
+const ApiError = require('../exceptions/api-error')
 
 class UserController {
     async registration(req, res, next) {
         try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Ошибка при валидации', errors.array()))
+            }
             const { email, password } = req.body
             const userData = await userService.registration(email, password)
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true}) //добавить secure: true для https
@@ -15,7 +20,10 @@ class UserController {
 
     async login(req, res, next) {
         try {
-            
+            const {email, password} = req.body
+            const userData = await userService.login(email, password)
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true}) //добавить secure: true для https
+            return res.json(userData)
         } catch (error) {
             next(error)
         }
@@ -23,7 +31,10 @@ class UserController {
 
     async logout(req, res, next) {
         try {
-            
+            const {refreshToken} = req.cookies
+            const token = await userService.logout(refreshToken)
+            res.clearCookie('refreshToken')
+            return res.json(token)
         } catch (error) {
             next(error)
         }
@@ -41,7 +52,10 @@ class UserController {
 
     async refresh(req, res, next) {
         try {
-            
+            const {refreshToken} = req.cookies
+            const userData = await userService.refresh(refreshToken)
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true}) //добавить secure: true для https
+            return res.json(userData)
         } catch (error) {
             next(error)
         }
@@ -49,7 +63,8 @@ class UserController {
 
     async getUsers(req, res, next) {
         try {
-            res.json(['123', '456'])
+            const users = await userService.getAllUsers()
+            return res.json(users)
         } catch (error) {
             next(error)
         }
--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,14 @@
-const Router = require('express');
+const Router = require('express')
 const userController = require('../controllers/user-controller')
-const router = new Router();
+const router = new Router()
+const { body } = require('express-validator')
+const authMiddleware = require('../middlewares/auth-middleware')
 
-router.post('/registration', userController.registration);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-router.get('/activate/:link', userController.activate);
-router.get('/refresh', userController.refresh);
-router.get('/users', userController.getUsers);
+router.post('/registration', body('email').isEmail(), body('password').isLength({min: 6, max: 16}), userController.registration)
+router.post('/login', userController.login)
+router.post('/logout', userController.logout)
+router.get('/activate/:link', userController.activate)
+router.get('/refresh', userController.refresh)
+router.get('/users', authMiddleware, userController.getUsers)
 
-module.exports = router;
+module.exports = router     
